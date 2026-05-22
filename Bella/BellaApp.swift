b19eb2ff9dcc0dc674813d40ae5fa916166ccbc5
bellarita <125526589+bellarita@users.noreply.github.com>
2026-05-22 23:08:48 +0200

    Initial Commit

---
+++ Bella/BellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellaApp.swift
+//  Bella
+//
+//  Created by 漂亮大宝贝 on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bella/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bella
+//
+//  Created by 漂亮大宝贝 on 22/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
